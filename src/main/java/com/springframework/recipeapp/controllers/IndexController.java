d69b7c40c0498a0052062be180cace500f926533
Serhat Tan <94serhattan@gmail.com>
2019-03-01 10:03:04 +0300

    Added recipe service and modified index html

--- src/main/java/com/springframework/recipeapp/controllers/IndexController.java
+++ src/main/java/com/springframework/recipeapp/controllers/IndexController.java
@@ -1,33 +1,22 @@
 package com.springframework.recipeapp.controllers;
 
-import com.springframework.recipeapp.controllers.model.Category;
-import com.springframework.recipeapp.controllers.model.UnitOfMeasure;
-import com.springframework.recipeapp.controllers.repositories.CategoryRepository;
-import com.springframework.recipeapp.controllers.repositories.UnitOfMeasureRepository;
+import com.springframework.recipeapp.controllers.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "index"})
-    public String getIndexPage() {
-
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByUom("Teaspoon");
-
-        System.out.println("Cat Id is:" + categoryOptional.get().getId());
-        System.out.println("UOM Id is:" + unitOfMeasureOptional.get().getId());
+    public String getIndexPage(Model model) {
+        model.addAttribute("recipes", recipeService.getRecipe());
 
         return "index";
     }
---
+++ src/main/java/com/springframework/recipeapp/controllers/services/RecipeService.java
@@ -0,0 +1,10 @@
+package com.springframework.recipeapp.controllers.services;
+
+import com.springframework.recipeapp.controllers.model.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService {
+
+    Set<Recipe> getRecipe();
+}

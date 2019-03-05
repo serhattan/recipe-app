864f077fdd89b5cd2402b1e37d5fc759e99ccf8e
Serhat Tan <94serhattan@gmail.com>
2019-03-05 11:42:17 +0300

    Refactored recipe relation with lombok

--- src/main/java/com/springframework/recipeapp/controllers/model/Note.java
+++ src/main/java/com/springframework/recipeapp/controllers/model/Note.java
@@ -1,10 +1,12 @@
 package com.springframework.recipeapp.controllers.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
 @Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Note {
 

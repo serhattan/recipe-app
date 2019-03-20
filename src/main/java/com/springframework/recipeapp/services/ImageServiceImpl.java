c018d3a243771eca115d5fbe1225757b64d0fd2d
Serhat Tan <94serhattan@gmail.com>
2019-03-20 10:55:12 +0300

    Saved image into database

--- src/main/java/com/springframework/recipeapp/services/ImageServiceImpl.java
+++ src/main/java/com/springframework/recipeapp/services/ImageServiceImpl.java
@@ -1,15 +1,43 @@
 package com.springframework.recipeapp.services;
 
+import com.springframework.recipeapp.model.Recipe;
+import com.springframework.recipeapp.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
 
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
     public void saveImageFile(Long recipeId, MultipartFile file) {
-        log.debug("Received a file");
+
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i= 0;
+
+            for (byte b: file.getBytes()) {
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            log.error("Error occured", e);
+
+            e.printStackTrace();
+        }
     }
 }
